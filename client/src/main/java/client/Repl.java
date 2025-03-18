e525f0627e45fb584a19871635e0d84f872ec346
Cooper Motyer <cjmotyer@gmail.com>
2025-03-17 22:50:35 -0600

    added base Repl class

---
+++ client/src/main/java/client/Repl.java
@@ -0,0 +1,38 @@
+package client;
+
+import java.util.Scanner;
+
+import static client.EscapeSequences.*;
+
+public class Repl {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("Welcome to chess! Sign in or register to start.");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        String result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(BLUE + result);
+            } catch (Exception e) {
+                String msg = e.getMessage();
+                System.out.print(RED + msg);
+            }
+        }
+        System.out.println();
+    }
+
+    public void printPrompt() {
+        System.out.print("\n" + RESET + ">>> " + GREEN);
+    }
+}
